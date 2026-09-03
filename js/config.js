9d9cb1e441fd166ee93d7b7da9f12b6ca16e0c3a
sia-tolf <tolf@velbnk.is>
2026-09-03 16:03:44 +0500

    Add server-specific Local ID options and defaults

--- js/config.js
+++ js/config.js
@@ -1,23 +1,35 @@
 const API = "https://api.tolf.is";
 
 const SERVERS = {
-  riga: {
-    host: "ikev2-riga.tolf.is",
-    nameKey: "rigaLatvia"
-  },
-
-  moscow: {
-    host: "ikev2.tolf.is",
-    nameKey: "moscowRussia"
-  },
-
-  uk: {
-    host: "",
-    nameKey: "londonUk"
-  }
+  riga: { host: "ikev2-riga.tolf.is", nameKey: "rigaLatvia" },
+  moscow: { host: "ikev2.tolf.is", nameKey: "moscowRussia" },
+  uk: { host: "", nameKey: "londonUk" }
 };
 
 const I18N = {};
+
+const LOCAL_ID_OPTIONS = {
+  riga: {
+    sr: "routingRigaSr",
+    ru: "routingRu"
+  },
+  moscow: {
+    "": "routingMoscowDefault",
+    sr: "routingMoscowSr",
+    ru: "routingRu",
+    lv: "routingLv"
+  }
+};
+
+const LOCAL_ID_DEFAULTS = {
+  riga: "sr",
+  moscow: ""
+};
+
+let localIdValues = {
+  riga: "",
+  moscow: ""
+};
 
 let currentLanguage = localStorage.getItem("tolfLanguage");
 
