97bf7f7d6ddcc76070bff78ca3bcf30495f21e01
sia-tolf <tolf@velbnk.is>
2026-09-04 22:07:04 +0500

    Enable Latvian language button

--- js/app.js
+++ js/app.js
@@ -1,2 +1,17 @@
+languageEn.addEventListener(
+  "click",
+  () => setLanguage("en")
+);
+
+languageRu.addEventListener(
+  "click",
+  () => setLanguage("ru")
+);
+
+languageLv.addEventListener(
+  "click",
+  () => setLanguage("lv")
+);
+
 setLanguage(currentLanguage);
 loadAccount();
